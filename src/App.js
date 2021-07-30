59669002fde32597df8d3382dc5ad2601db91240
Gautam2404 <barfagautam@gmail.com>
2021-07-30 18:43:59 +0530

    please push code

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Login from './components/login';
+import Navbar from './components/Navbar';
+function App() {
+  return (
+    <div className="App">
+      {/* <Login /> */}
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
